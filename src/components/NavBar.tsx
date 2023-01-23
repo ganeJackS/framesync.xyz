8739cd172d38196091b830b1097337c849b15a20
swervinsmurf <38080344+ganeJackS@users.noreply.github.com>
2023-01-22 20:28:12 -0500

    add link for framesync.xyz

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,15 @@ import framesync from "../assets/framesync.svg";
 
 const NavBar = () => {
   return (
+    <>
+    <span className="flex flex-row text-2xl text-white bg-slate-800 justify-center">Latest version now live on:{" "}<a
+    className="text-blue-500 pl-1"
+    href="https://framesync.xyz" target={"_blank"}>{" "}framesync.xyz</a></span>
+    
     <div className="flex flex-col justify-center  bg-darkest-blue pt-4 pl-6 pb-2">
       <div className="flex flex-row self-stretch bg-darkest-blue">
         <a href="#" className="mr-6 self-stretch text-4xl">
-          <img src={framesync} alt="framesync logo" />
+          <img src={framesync} alt="framesync logo" /> 
         </a>
         <div>
           <ul className="flex flex-row space-x-4">
@@ -14,6 +19,7 @@ const NavBar = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
