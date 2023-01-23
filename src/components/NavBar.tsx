a604f7aaf4245975fea7dba6a2d0f01b3a9189f6
swervinsmurf <38080344+ganeJackS@users.noreply.github.com>
2023-01-22 20:41:22 -0500

    fix?

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,12 @@ import framesync from "../assets/framesync.svg";
 const NavBar = () => {
   return (
     <>
-    <span className="flex flex-row text-2xl font-mono text-white bg-gray-800 justify-center">Latest version now live on {" "}<a
-    className="text-orange-500 pl-3"
-    href="https://framesync.xyz" target={"_blank"}>{" "}https://framesync.xyz</a></span>
+   
     
     <div className="flex flex-col justify-center  bg-darkest-blue pt-4 pl-6 pb-2">
+    <span className="flex flex-row text-2xl mb-4 font-mono text-white bg-gray-800 justify-center">Latest version now live on {" "}<a
+    className="text-orange-500 pl-3"
+    href="https://framesync.xyz" target={"_blank"}>{" "}https://framesync.xyz</a></span>
       <div className="flex flex-row self-stretch bg-darkest-blue">
         <a href="#" className="mr-6 self-stretch text-4xl">
           <img src={framesync} alt="framesync logo" /> 
