fe6f4ffe742b5381012be9a1cf1ecdc7fc0150b8
swervinsmurf <38080344+ganeJackS@users.noreply.github.com>
2023-01-23 19:57:46 -0500

    fix vercel insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import App from './App'
 import './index.css'
 import { inject } from '@vercel/analytics';
 
+inject();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
 
-inject();
