0121c901c8dd5a6b1a05a82774b4fa75f9244a83
swervinsmurf <38080344+ganeJackS@users.noreply.github.com>
2023-01-24 00:56:09 -0500

    yeezy

--- src/hooks/useAudio2Keyframes.tsx
+++ src/hooks/useAudio2Keyframes.tsx
@@ -66,12 +66,21 @@ function useAudio2Keyframes(
           let channel2Val = audioBuffer
             .getChannelData(1)
             .slice(start, end)
-            .map((x) => Math.abs(x))
-            .reduce((acc, val) => acc + val);
+            .map((x) => {
+              console.log("x: ", x);
+              console.log("Math.abs(x): ", Math.abs(x));
+              return Math.abs(x);
+            })
+            .reduce((acc, val) => {
+              console.log("acc: ", acc);
+              console.log("val: ", val);
+              console.log("sum: ", acc + val);
+              return acc + val;
+            });
 
-          console.log("c1: ", channel1Val);
-          console.log("c2: ", channel2Val);
-          console.log("sum: ", channel1Val + channel2Val);
+          // console.log("c1: ", channel1Val);
+          // console.log("c2: ", channel2Val);
+          // console.log("sum: ", channel1Val + channel2Val);
 
           return channel1Val + channel2Val;
         });
