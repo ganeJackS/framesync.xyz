24dcd7fb0fa5eecc229fb40c0940ec0af0209f1e
swervinsmurf <38080344+ganeJackS@users.noreply.github.com>
2023-01-23 20:19:57 -0500

    checking useEffect audioBuffer

--- src/hooks/useAudio2Keyframes.tsx
+++ src/hooks/useAudio2Keyframes.tsx
@@ -1,37 +1,43 @@
 import { useEffect, useState } from "react";
 
 function useAudio2Keyframes(audioBuffer: AudioBuffer, frameRate: number) {
-  const [keyframes, setKeyframes] = useState<number[]>([]);
-
+  
   useEffect(() => {
-    if (!audioBuffer || !frameRate) return;
-
-    const duration = audioBuffer.duration;
-    const numFrames = duration * frameRate;
-    const chunkSize = Math.floor(audioBuffer.length / numFrames);
-    const chunkValues: number[] = [];
-
-    for (let i = 0; i < numFrames; i++) {
-      let chunkValue = 0;
-      for (let j = i * chunkSize; j < (i + 1) * chunkSize; j++) {
-        chunkValue += Math.abs(audioBuffer.getChannelData(0)[j]);
-      }
-      if (!chunkValue) {
-        chunkValue = 0;
-      }
-      chunkValues.push(chunkValue);
-    }
-    console.log("chunkValues", chunkValues)
-    const normalizedChunkValues = chunkValues.map(
-      (value) => value / Math.max(...chunkValues)
-    );
-    console.log("normalizedChunkValues", normalizedChunkValues)
-
-    setKeyframes(normalizedChunkValues);
+    console.log(audioBuffer);
+    console.log(frameRate);
   }, [audioBuffer, frameRate]);
   
-  console.log("audio2keyframes", keyframes)
-  return keyframes;
+  //const [keyframes, setKeyframes] = useState<number[]>([]);
+
+  // useEffect(() => {
+  //   if (!audioBuffer || !frameRate) return;
+
+  //   const duration = audioBuffer.duration;
+  //   const numFrames = duration * frameRate;
+  //   const chunkSize = Math.floor(audioBuffer.length / numFrames);
+  //   const chunkValues: number[] = [];
+
+  //   for (let i = 0; i < numFrames; i++) {
+  //     let chunkValue = 0;
+  //     for (let j = i * chunkSize; j < (i + 1) * chunkSize; j++) {
+  //       chunkValue += Math.abs(audioBuffer.getChannelData(0)[j]);
+  //     }
+  //     if (!chunkValue) {
+  //       chunkValue = 0;
+  //     }
+  //     chunkValues.push(chunkValue);
+  //   }
+  //   console.log("chunkValues", chunkValues)
+  //   const normalizedChunkValues = chunkValues.map(
+  //     (value) => value / Math.max(...chunkValues)
+  //   );
+  //   console.log("normalizedChunkValues", normalizedChunkValues)
+
+  //   setKeyframes(normalizedChunkValues);
+  // }, [audioBuffer, frameRate]);
+  
+  // console.log("audio2keyframes", keyframes)
+  // return keyframes;
   
 }
 
