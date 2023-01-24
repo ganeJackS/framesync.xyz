4a58d1c74951aba1b20997552dce5324a5b42c2e
swervinsmurf <38080344+ganeJackS@users.noreply.github.com>
2023-01-23 20:41:04 -0500

    for loop rewrite w/ timeout

--- src/hooks/useAudio2Keyframes.tsx
+++ src/hooks/useAudio2Keyframes.tsx
@@ -5,35 +5,33 @@ function useAudio2Keyframes(audioBuffer: AudioBuffer, frameRate: number) {
 
   useEffect(() => {
     if (!audioBuffer || !frameRate) return;
-
-      console.log(audioBuffer);
-      console.log(frameRate);
-
     const duration = audioBuffer.duration;
     const numFrames = duration * frameRate;
     const chunkSize = Math.floor(audioBuffer.length / numFrames);
     const chunkValues: number[] = [];
-
-    // for (let i = 0; i < numFrames; i++) {
-    //   let chunkValue = 0;
-    //   for (let j = i * chunkSize; j < (i + 1) * chunkSize; j++) {
-    //     chunkValue += Math.abs(audioBuffer.getChannelData(0)[j]);
-    //   }
-    //   if (!chunkValue) {
-    //     chunkValue = 0;
-    //   }
-    //   chunkValues.push(chunkValue);
-    // }
-    console.log("chunkValues", chunkValues)
-    // const normalizedChunkValues = chunkValues.map(
-    //   (value) => value / Math.max(...chunkValues)
-    // );
-    // console.log("normalizedChunkValues", normalizedChunkValues)
-
-    setKeyframes(chunkValues);
+    let i = 0;
+    function processChunk(){
+        let chunkValue = 0;
+        for (let j = i * chunkSize; j < (i + 1) * chunkSize; j++) {
+            chunkValue += Math.abs(audioBuffer.getChannelData(0)[j]);
+        }
+        if (!chunkValue) {
+            chunkValue = 0;
+        }
+        chunkValues.push(chunkValue);
+        i++;
+        if(i<numFrames){
+            setTimeout(processChunk,0)
+        }else{
+            const normalizedChunkValues = chunkValues.map(
+            (value) => value / Math.max(...chunkValues)
+            );
+            setKeyframes(normalizedChunkValues);
+        }
+    }
+    processChunk()
   }, [audioBuffer, frameRate]);
-  
-  console.log("audio2keyframes", keyframes)
+
   return keyframes;
   
 }
